c1d1d8e1c12359a5830f109898aec4062b638b90
Ben Lesh <blesh@netflix.com>
2016-04-20 16:18:20 -0700

    chore(init): initial commit

---
+++ src/RxDucksComponent.js
@@ -0,0 +1,7 @@
+import { Component } from 'react';
+
+export class RxDucksComponent extends Component {
+  constructor(props) {
+    super(props);
+  }
+}
---
+++ test/RxDucksComponent-spec.js
@@ -0,0 +1,9 @@
+/* globals describe, it */
+import { RxDucksComponent } from '../';
+import { expect } from 'chai';
+
+describe('RxDucksComponent', () => {
+  it('should exist', () => {
+    expect(RxDucksComponent).to.be.a('function');
+  });
+});
