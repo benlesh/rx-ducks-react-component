7dd86c5ad36cfeca361292bd06086baaeeb0591b
Ben Lesh <blesh@netflix.com>
2016-04-20 19:53:52 -0700

    feat(RxDucksComponent): initial implementation working and primitive test

--- src/RxDucksComponent.js
+++ src/RxDucksComponent.js
@@ -1,7 +1,33 @@
 import { Component } from 'react';
+import { rxDucksMiddleware } from 'rx-ducks-middleware';
+import { Subscription } from 'rxjs/Subscription';
+import { createStore, applyMiddleware } from 'redux';
 
 export class RxDucksComponent extends Component {
   constructor(props) {
     super(props);
+    this._rxducks = rxDucksMiddleware();
+  }
+
+  get store() {
+    if (!this._store) {
+      this._store = createStore(this.stateReducer, applyMiddleware(this._rxducks.middleware));
+    }
+    return this._store;
+  }
+
+  componentDidMount() {
+    const { actions, send } = this._rxducks;
+    const { store } = this;
+    const subscription = new Subscription();
+    subscription.add(this.interactions()
+      .subscribe(action => store.dispatch(action)));
+    subscription.add(this.sideEffects(actions)
+      .subscribe(send));
+    this._rxducks_sub = subscription;
+  }
+
+  componentDidUnmount() {
+    this._rxducks_sub.unsubscribe();
   }
 }
--- test/RxDucksComponent-spec.js
+++ test/RxDucksComponent-spec.js
@@ -2,8 +2,62 @@
 import { RxDucksComponent } from '../';
 import { expect } from 'chai';
 
+import { Subject } from 'rxjs/Subject';
+import { filter } from 'rxjs/operator/filter';
+import { map } from 'rxjs/operator/map';
+
 describe('RxDucksComponent', () => {
   it('should exist', () => {
     expect(RxDucksComponent).to.be.a('function');
+  });
+
+  it('should set up middleware', () => {
+    class MyComponent extends RxDucksComponent {
+    }
+
+    let comp = new MyComponent();
+    expect(comp._rxducks).to.be.a('object');
+  });
+
+  it('integration 1', () => {
+    const reducedActions = [];
+
+    class MyComponent extends RxDucksComponent {
+      clicks = new Subject();
+
+      interactions() {
+        return this.clicks;
+      }
+
+      sideEffects(actions) {
+        return actions::filter(({ type }) => type === 'CLICK')
+          ::map((_, index) => ({ type: 'CLICKED', index }));
+      }
+
+      stateReducer(state, action) {
+        reducedActions.push(action);
+      }
+
+      clickMe() {
+        this.clicks.next({ type: 'CLICK' });
+      }
+
+      render() {
+        return (<button onClick={this.clickMe.bind(this)}>click me</button>);
+      }
+    }
+
+    let comp = new MyComponent();
+
+    expect(comp._rxducks).to.be.a('object');
+
+    comp.componentDidMount();
+
+    comp.clickMe();
+    comp.clickMe();
+
+    expect(reducedActions).to.deep.equal([{ type: '@@redux/INIT' }, { type: 'CLICKED', index: 0 }, { type: 'CLICKED', index: 1 }]);
+
+    comp.componentDidUnmount();
   });
 });
